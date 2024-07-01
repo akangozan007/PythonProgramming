334ec1ec3420f8f203c7576accb98f40a362096f
root <root@portofolioku.com>
2024-07-01 14:46:00 -0400

    Menegupload

---
+++ pengenalan-python/pengenalan_syntax_python.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# KATA KATA MUTIARA
+# Variabel yang Valid
+# Syarat variabel yang valid di python:
+
+# Nama variabel panjangnya minimal 1 huruf,
+# Menggunakan huruf (besar atau kecil), underscore _, atau angka 0 hingga 9,
+# Case sensitif, artinya variabel SAYA dan saya itu berbeda,
+# Variabel tidak boleh dimulai dari angka.
+# Variabel boleh dimulai dari underscore _, tetapi di CodeSaya dilarang untuk keamanan.
+
+nama_bahasa = "python"
+
+P3MBU4T = "Guido van Rossum" # nama variabel yang 'alay', tapi apakah valid?
+
+Tahun_dibuatnya = 1991
+
+
+# variabel panjang nih
+contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, CodeSaya, dan lain-lain."
+
+belajar_python_di_codesaya = True
+
+# uji coba mencetak nilai yang berada didalam variable
+print(belajar_python_di_codesaya)
+print(nama_bahasa)
+print(P3MBU4T)
+print(Tahun_dibuatnya)
+
