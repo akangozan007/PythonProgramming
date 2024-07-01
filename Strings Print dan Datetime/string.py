4d48c8ac774e98a34ae5d1092b2d8c6fbbb51e22
root <root@portofolioku.com>
2024-07-01 16:37:47 -0400

    Mempelajari String

---
+++ Strings Print dan Datetime/string.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# KATA KATA MUTIARA
+
+# STRING
+# Lagi Metode String
+# Saatnya mengimplementasikan apa yang telah anda pelajari di unit sebelumnya!
+
+#  Instruksi:
+# Lengkapi variabel di editor samping menggunakan metode yang telah kita pelajari sebelumnya. Lihat petunjuk atau unit sebelumnya jika kesulitan.
+
+ucapan = "Nama Python dipilih karena pembuatnya menyukai acara TV Monty Python."
+
+# panjang ucapan simpan di sini dengan len()
+panjang = len(ucapan)
+# Mencetak variable panjang
+print(panjang)
+# hasilkan huruf besar semua dengan upper()
+besar = ucapan.upper()
+# print variable besar
+print(besar)
+# hasilkan huruf kecil semua dengan lower()
+kecil = ucapan.lower()
+# print variable kecil
+print(kecil)
