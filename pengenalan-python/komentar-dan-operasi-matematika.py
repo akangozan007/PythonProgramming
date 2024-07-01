31c977aab746967ab4129568fc484467bb8e2ebd
root <root@portofolioku.com>
2024-07-01 14:55:50 -0400

    Mengupload komentar-dan-operasi-matematika.py

---
+++ pengenalan-python/komentar-dan-operasi-matematika.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# KATA KATA MUTIARA
+# Kesimpulan
+# Selamat anda sudah sampai pada unit ini! Bagaimana pendapat anda tentang Python? Mudah kan!? Jangan salah, walaupun mudah bukan berarti Python itu lemah! Dengan python anda bisa buat apa saja! Mari kita ulas apa saja yang telah kita pelajari:
+
+# Variabel: untuk menyimpan data,
+# Tipe data: boolean dan angka,
+# Spasi: pentingnya spasi di python,
+# Komentar: untuk menjelaskan code,
+# Operasi matematika: mulai dari penambahan sampai modulus!
+
+#ini adalah komentar
+jumlah_pacar = 5
+lagi_galau = True
+obito = 10 * 5
+
+print(jumlah_pacar)
+print(lagi_galau)
+print(obito)
