7f0db7fd87953c97d5eed9ced838511b3399b116
root <root@portofolioku.com>
2024-07-02 05:34:16 -0400

    Mengupload Print.py

--- Strings Print dan Datetime/print.py
+++ Strings Print dan Datetime/print.py
@@ -7,5 +7,20 @@
 # Saatnya mereview apa yang telah anda pelajari!
 
 #  Instruksi:
-# Lihat code di samping. Itu semua kita pelajari di sub-bab ini loh! Ubah sesuka hati anda, kali ini tidak diperiksa! Jika sudah siap dan mengerti, silahkan menuju unit berikutnya!
+# Lihat code di samping. Itu semua kita pelajari di sub-bab ini loh!
+# Ubah sesuka hati anda, kali ini tidak diperiksa! Jika sudah siap dan mengerti,
+#  silahkan menuju unit berikutnya!
 
+selain_itu = "juga sudah bisa mencetak variabel..."
+
+print(selain_itu)
+
+print("Merangkai " + "kata " + "menjadi " + "kalimat...")
+
+print("Bahkan angka seperti " + str(1) + ", " + str(2) + ", dst pun dirangkai...")
+
+anda = "Anda"
+
+terhebat = "terhebat"
+
+print("Apakah {0} tahu siapa yang {1}? {0}! {0}!".format(anda, terhebat))
